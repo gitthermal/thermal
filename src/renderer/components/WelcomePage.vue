98fcf688fa63fb0692e5addc0124c99ea3ddc60f
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-13 18:01:10 +0530

    Create WelcomePage.vue

---
+++ src/renderer/components/WelcomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome to Thermal</h1>
+    <p>One stop for all your Git repository.</p>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang='sass'>
+
+</style>
