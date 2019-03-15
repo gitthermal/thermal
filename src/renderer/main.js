47d6b27f4d83e86e504df9412ac4851ae5dd55d7
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-15 23:15:09 +0530

    Import base stylesheet

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,7 @@ import axios from "axios"
 import App from "./App"
 import router from "./router"
 import store from "./store"
+import "./assets/css/base.sass"
 
 if (!process.env.IS_WEB) Vue.use(require("vue-electron"))
 Vue.http = Vue.prototype.$http = axios
