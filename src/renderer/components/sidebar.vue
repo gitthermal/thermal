07b9f6ab0a626c14657ebfd5e8d4921f04fa7b40
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-16 00:23:19 +0530

    Create sidebar component layout

---
+++ src/renderer/components/sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sidebar">
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang='sass'>
+	.sidebar
+		background-color: #EFEFEF
+		padding-top: 20px
+		border-right: 1px solid #DEE0E3
+		padding-bottom: 20px
+</style>
