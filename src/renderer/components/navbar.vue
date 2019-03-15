662bb29cfd44bdde7b847e18eba861be4a9ffa77
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-16 00:25:44 +0530

    Create navbar component layout

---
+++ src/renderer/components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang='sass'>
+	.navbar
+		background-color: #EFEFEF
+		border-top: 1px solid #DEE0E3
+		border-bottom: 1px solid #DEE0E3
+		padding: 12px 20px
+</style>
