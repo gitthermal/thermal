c05cd5935a6f4a2c9e6dff062454e7f47f9e1c59
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-16 00:32:53 +0530

    Add Workspace item + folder icon

--- src/renderer/components/sidebar.vue
+++ src/renderer/components/sidebar.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="sidebar">
+    <div class="sidebar--group">
+      <router-link class="sidebar--item" to="workspace">
+        <folderIcon/>
+        <p>Workspace</p>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import folderIcon from "./icon/folder";
 export default {
+  components: {
+    folderIcon,
+  }
 };
 </script>
 
@@ -14,4 +24,25 @@ export default {
 		padding-top: 20px
 		border-right: 1px solid #DEE0E3
 		padding-bottom: 20px
+
+	.sidebar--group, .sidebar--item
+		display: flex
+
+	.sidebar--group
+		flex-direction: column
+
+	.sidebar--item
+		padding: 3px 20px
+		margin-bottom: 5px
+		align-items: center
+
+		svg
+			width: 20px
+			height: 20px
+			stroke: #6C6F75
+			margin-right: 10px
+
+		p
+			color: #6C6F76
+			font-size: 14px
 </style>
