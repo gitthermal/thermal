495bf4892822d17587b06de5f6b44175466f4799
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-15 23:59:50 +0530

    Add welcome icon
    Add welcome icons
    
    * Website
    * Twitter
    * Coffee

--- src/renderer/components/WelcomePage.vue
+++ src/renderer/components/WelcomePage.vue
@@ -2,6 +2,17 @@
   <div>
     <h1>Welcome to Thermal</h1>
     <p class="welcome--description">One stop for all of your Git repository.</p>
+    <div class="welcome--icon">
+      <div class="welcome--item-icon">
+        <linkIcon/>
+      </div>
+      <div class="welcome--item-icon">
+        <twitterIcon/>
+      </div>
+      <div class="welcome--item-icon">
+        <coffeeIcon/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,4 +25,17 @@ export default {
 	.welcome--description
 		margin-bottom: 1rem
 
+	.welcome--icon
+		display: flex
+
+	.welcome--item-icon
+		cursor: pointer
+		
+		svg
+			width: 30px
+			height: 30px
+			stroke: #222831
+
+		&:not(:last-child)
+			margin-right: 20px
 </style>
