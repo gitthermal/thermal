ea4bb2f9bfd305fdcbe39fff6d1da67bb416a07f
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-16 21:28:55 +0530

    Create history page layout

---
+++ src/renderer/components/history.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="commit--history">
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang='sass'>
+	.commit--history
+		border-right: 1px solid #DEE0E3
+		width: 250px
+		overflow: hidden
+		overflow-y: scroll
+		display: flex
+		flex-direction: row
+</style>
