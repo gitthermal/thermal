d058be8e2ab76df96dfdf0aa4afb4828cbaea6d5
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-16 18:43:10 +0530

    Create inputText field component

--- src/renderer/components/addRepo.vue
+++ src/renderer/components/addRepo.vue
@@ -15,11 +15,13 @@
     </div>
     <div class="model--section model--footer">
       <primaryButton class="model--footer-primaryButton" text="Add repository"/>
+			<button @click="addRepository()">Add repo</button>
     </div>
   </div>
 </template>
 
 <script>
+// C:/Users/YASHU/Desktop/vscode
 import closeIcon from "./icon/close"
 import inputText from "./inputText"
 import primaryButton from "./primaryButton"
@@ -38,8 +40,15 @@ export default {
 	},
 	methods: {
 		addRepository() {
+			console.log(this.pathToRepository.toString().split("/"))
 			let repoName = this.pathToRepository.split("/")[ this.pathToRepository.split("/").length - 1 ]
+
+			console.log(repoName)
+
+			console.log(this.$store._actions)
+
 			this.$store.dispatch("addRepo/addLocalRepo")
+			console.log({ ...this.$store.state.addRepo.repo })
 		}
 	}
 }
---
+++ src/renderer/components/inputText.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="text"
+    :name="name"
+    class="input--field-text"
+    :placeholder="placeholder"
+		@input="$emit('input', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+	name: "inputText",
+	props: {
+		name: {
+			type: String
+		},
+		placeholder: {
+			type: String
+		}
+	}
+}
+</script>
+
+<style lang='sass'>
+	.input--field-text
+		border-radius: 3px
+		padding: .5rem .8rem
+		border: 1px solid #eeeeee
+		font-family: inherit
+		outline: none
+		width: 100%
+</style>
