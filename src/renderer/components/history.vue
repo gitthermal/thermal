eab02b61ff352e7602575b5b6d1f740ecb610b6c
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-18 13:54:19 +0530

    Increase history width to 300px

--- src/renderer/components/history.vue
+++ src/renderer/components/history.vue
@@ -100,7 +100,7 @@ export default {
 <style lang='sass'>
 	.commit--history
 		border-right: 1px solid #DEE0E3
-		width: 250px
+		width: 300px
 		overflow: hidden
 		overflow-y: scroll
 		display: flex
