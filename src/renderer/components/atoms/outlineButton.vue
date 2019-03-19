311bf2e3717e7529594e5da35660ef11216ece12
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-20 00:57:14 +0530

    Create outlineButton component

---
+++ src/renderer/components/atoms/outlineButton.vue
@@ -0,0 +1,21 @@
+<template>
+	<button class="button button--outline">{{ text }}</button>
+</template>
+
+<script>
+	export default {
+		props: {
+			text: {
+				type: String,
+				required: true
+			}
+		}
+	}
+</script>
+
+<style lang='sass'>
+	.button--outline
+		border: 1px solid #00adb5
+		color: #00adb5
+		background: none
+</style>
