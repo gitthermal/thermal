074ec86460297291fe54688dabdcc1f92e9e26e9
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-21 17:48:09 +0530

    Move commit detail to commitInformation.vue component

--- src/renderer/components/history.vue
+++ src/renderer/components/history.vue
@@ -5,45 +5,6 @@
     </div>
     <div v-show="this.$store.state.history.commitInformation.isActive" class="commit__detail">
       <div class="commit__detail__buttons">
-      <div class="commit__detail__summary">
-        <div class="commit__detail__summary__title">{{ this.commitDetail.title }}</div>
-        <div class="commit__detail__summary__description">{{ this.commitDetail.description }}</div>
-      </div>
-      <div class="commit__detail__meta">
-        <div class="commit__detail__meta__item">Author:
-          <p>{{ this.commitDetail.author.name }} <{{ this.commitDetail.author.email }}></p>
-        </div>
-        <div class="commit__detail__meta__item">Author Date:
-          <p>{{ this.commitDetail.author.date }}</p>
-        </div>
-        <div class="commit__detail__meta__item">Committer:
-          <p>{{ this.commitDetail.committer.name }} <{{ this.commitDetail.committer.email}}></p>
-        </div>
-        <div class="commit__detail__meta__item">Committer Date:
-          <p>{{ this.commitDetail.committer.date }}</p>
-        </div>
-        <div class="commit__detail__meta__item">Refs:
-          <p>{{ this.commitDetail.refs }}</p>
-        </div>
-        <div class="commit__detail__meta__item">Commit Hash:
-          <p>{{ this.commitDetail.hash }}</p>
-        </div>
-        <div class="commit__detail__meta__item">Tree Hash:
-          <p>{{ this.commitDetail.tree_hash }}</p>
-        </div>
-        <div class="commit__detail__meta__item">Parent Hash:
-          <p>{{ this.commitDetail.parent_hash }}</p>
-        </div>
-      </div>
-      <div class="commit__detail__files">
-        <div
-          class="commit__detail__files__summary"
-        >Showing {{ this.commitDetail.fileList.length }} changed files with {{ this.commitDetail.meta.additions ? this.commitDetail.meta.additions : '0' }} additions and {{ this.commitDetail.meta.deletion ? this.commitDetail.meta.additions : '0' }} deletion</div>
-        <div
-          class="commit__detail__files__list"
-          v-for="file in this.commitDetail.fileList"
-          :key="file"
-        >{{ file.replace(/\|.*/, '').trim() }}</div>
         <div @click="toggleCommitDetail()" class="commit__detail__buttons__back">Back</div>
       </div>
 			<commitInformation />
@@ -78,9 +39,6 @@ export default {
 				console.log(error)
 			}
 		},
-			this.getCommitBody(hash)
-			this.getCommitMeta(hash)
-			this.getFilesDetail(hash)
 		toggleCommitDetail() {
 			this.$store.dispatch("history/showCommitInformation")
 		},
@@ -89,86 +47,6 @@ export default {
 			this.$store.dispatch("history/updateCommitInformationCommitHash", {
 				commit_hash: hash
 			})
-		},
-		async getCommitBody(hash) {
-			const content = await git(this.repo).show([
-				hash,
-				"--format=%s %n << %n %b %n >>"
-			])
-			try {
-				const output = content.split("\n")
-				this.commitDetail.title = output[0]
-				let commitDescriptionStart
-				let commitDescriptionEnd
-				for (let i = 0; i < output.length; i++) {
-					if (output[i].trim() === "<<") {
-						commitDescriptionStart = i
-					}
-					if (output[i].trim() === ">>") {
-						commitDescriptionEnd = i
-					}
-				}
-				if (commitDescriptionEnd - commitDescriptionStart > 2) {
-					this.commitDetail.description = output
-						.slice(commitDescriptionStart + 1, commitDescriptionEnd)
-						.toString()
-				}
-			} catch (error) {
-				console.log(error)
-			}
-		},
-		async getCommitMeta(hash) {
-			const content = await git(this.repo).show([
-				hash,
-				"--format=%ai %n %cn %n %ce %n %cd %n %d %n %T %n %P"
-			])
-			try {
-				const output = content.split("\n")
-				console.log(output)
-				this.commitDetail.author.date = output[0]
-				this.commitDetail.committer.name = output[1]
-				this.commitDetail.committer.email = output[2]
-				this.commitDetail.committer.date = output[3]
-				this.commitDetail.refs = output[4]
-				this.commitDetail.tree_hash = output[5]
-				this.commitDetail.parent_hash = output[6]
-			} catch (error) {
-				console.log(error)
-			}
-		},
-		async getFilesDetail(hash) {
-			const gitShowFiles = await git(this.repo).show([
-				hash,
-				"--oneline",
-				"--stat"
-			])
-			try {
-				let output = gitShowFiles.split("\n")
-
-				// list of file changes
-				this.commitDetail.fileList = output.slice(1, output.length - 2)
-				// number of file changes
-				this.commitDetail.meta.changes = this.commitDetail.fileList.length
-
-				const filesSummary = output[output.length - 1].split(", ")
-				filesSummary.shift()
-
-				for (let i = 0; i < filesSummary.length; i++) {
-					const commitMetaType = filesSummary[i].slice(
-						filesSummary[i].length - 2,
-						filesSummary[i].length - 1
-					)
-					const commitMetaNumber = filesSummary[i].split(" ")
-					if (commitMetaType === "+") {
-						this.commitDetail.meta.additions = commitMetaNumber[0]
-					}
-					if (commitMetaType === "-") {
-						this.commitDetail.meta.deletion = commitMetaNumber[0]
-					}
-				}
-			} catch (error) {
-				console.log(error)
-			}
 		}
 	},
 	mounted() {
@@ -183,80 +61,4 @@ export default {
 		width: 300px
 		overflow: hidden
 		overflow-y: scroll
-
-	.commit__detail
-
-		&__buttons
-			display: flex
-			padding: 10px
-			flex-direction: row
-			border-bottom: 1px solid #DEE0E3
-
-			&__back
-				font-size: 10px
-				cursor: pointer
-				color: #6C6F75
-				padding: 2px 6px
-				background-color: #DEE0E3
-				border-radius: 10px
-
-		&__author
-			padding: 10px
-			border-bottom: 1px solid #DEE0E3
-			display: flex
-			flex-direction: row
-
-			&__image
-				width: 50px
-				height: 50px
-				margin-right: 10px
-
-			&__name
-				margin-bottom: 0
-
-			&__email
-				font-size: 12px
-				color: #6C6F75
-
-		&__summary
-			padding: 10px
-			border-bottom: 1px solid #DEE0E3
-
-			&__description
-				margin-top: 6px
-				font-size: 12px
-
-		&__meta
-			padding: 10px
-			border-bottom: 1px solid #DEE0E3
-
-			&__item
-				font-size: 12px
-				color: #DEE0E3
-
-				&:not(:last-child)
-					margin-bottom: 4px
-
-				p
-					color: #2E3034
-
-		&__files
-
-			&__summary
-				padding: 10px
-				color: #DEE0E3
-				font-size: 10px
-				border-bottom: 1px solid #DEE0E3
-
-			&__list
-				border-bottom: 1px solid #DEE0E3
-				font-size: 12px
-				font-family: 'Space Mono', monospace
-				color: #2E3034
-				padding: 4px 10px
-				cursor: pointer
-
-				&:hover
-					color: white
-					background-color: #0366D6
 </style>
