a5c1e916c77f4525503fe2762d3cddbe2a3b892b
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-28 13:06:06 +0530

    Import vue2-scrollbar stylesheet globally

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,8 @@ import App from "./App"
 import router from "./router"
 import store from "./store"
 import "./assets/css/all.sass"
+// vue2-scrollbar styles
+import "vue2-scrollbar/dist/style/vue2-scrollbar.css"
 
 if (!process.env.IS_WEB) Vue.use(require("vue-electron"))
 Vue.http = Vue.prototype.$http = axios
