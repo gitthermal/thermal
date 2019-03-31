488c0b0f7b29aa6472834aa1d9f0d95e924cb1b4
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-31 19:19:54 +0530

    Create inputTextLabel.vue

---
+++ src/renderer/components/inputTextLabel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <label class="input__label" :for="labelName">{{ label }}</label>
+    <inputText
+      :name="name"
+      :placeholder="placeholder"
+			@input="$emit('input', $event)"
+    />
+  </div>
+</template>
+
+<script>
+import inputText from "./inputText"
+
+export default {
+	name: "inputTextLabel",
+	props: {
+		name: {
+			type: String
+		},
+		placeholder: {
+			type: String
+		},
+		label: {
+			type: String
+		}
+	},
+	components: {
+		inputText
+	},
+	computed: {
+		labelName() {
+			return this.label.toLowerCase().replace(" ", "_")
+		}
+	}
+}
+</script>
+
+<style lang='sass'>
+	.input__label
+		font-size: 12px
+		margin-bottom: .2rem
+</style>
