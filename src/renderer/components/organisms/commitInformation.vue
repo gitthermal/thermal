facfd7ba9cdc636f18e844e405b4d85ab34d3e22
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-31 14:02:33 +0530

    Add commit__detail class to root element

--- src/renderer/components/organisms/commitInformation.vue
+++ src/renderer/components/organisms/commitInformation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="commit__detail">
     <div class="commit__detail__author">
       <img class="commit__detail__author__image" src="../../../../static/image/user_avatar.png">
       <div>
