4c57c09b54c381ee49190fb7b6ad645fea5385a8
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-03 14:29:04 +0530

    Create newRemote.vue model layout

---
+++ src/renderer/components/model/newRemote.vue
@@ -0,0 +1,13 @@
+<template>
+	<div v-show="this.$store.state.model.model.newRemote" class="model--small">
+	</div>
+</template>
+
+<script>
+export default {
+	name: "newRemote",
+</script>
+
+<style lang='sass'>
+
+</style>
