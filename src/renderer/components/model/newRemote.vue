7da3f9083828048a8dbeda9c756dd0a867a62f3c
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-03 14:37:53 +0530

    Add newRemote header, input and primary button to model

--- src/renderer/components/model/newRemote.vue
+++ src/renderer/components/model/newRemote.vue
@@ -1,11 +1,56 @@
 <template>
 	<div v-show="this.$store.state.model.model.newRemote" class="model--small">
+		<div class="model__section model__header">
+      <h6 class="model__header__title">Add remote url</h6>
+      <div @click="closeModel()" class="model__header__close">
+        <closeIcon/>
+      </div>
+    </div>
+    <div class="model__section model__body flex-column">
+			<inputText
+				v-model="remoteUrl"
+				name="remoteUrl"
+				placeholder="Remote URL"
+				@keyup.esc.native="closeModel"
+				@keyup.enter.native="addRemoteUrl"
+				v-focus
+			/>
+    </div>
+    <div class="model__section model__footer">
+      <primaryButton class="ml-auto" text="Publish repository"/>
+    </div>
 	</div>
 </template>
 
 <script>
+import inputText from '../inputText'
+import closeIcon from '../icon/close'
+import primaryButton from '../atoms/primaryButton'
 export default {
 	name: "newRemote",
+	components: {
+		inputText,
+		closeIcon,
+		primaryButton
+	},
+	computed: {
+		currentRepository() {
+			return this.$store.getters["workspace/currentRepository"]
+		}
+	},
+	methods: {
+		closeModel() {
+			this.$store.dispatch("model/showNewRemote")
+		}
+	},
+	directives: {
+		focus: {
+			inserted: function (el) {
+				el.focus()
+			}
+		}
+	}
+}
 </script>
 
 <style lang='sass'>
