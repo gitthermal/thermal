4c5a2009469fa81435ddc14e5b758c63919a2db1
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-04 14:37:34 +0530

    Import settingsSidebar to settings route

---
+++ src/renderer/pages/settings/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="settings d-flex">
+    <settingsSidebar/>
+  </div>
+</template>
+
+<script>
+import settingsSidebar from "../../components/sidebar/settings"
+
+export default {
+	name: "settings",
+	components: {
+		settingsSidebar,
+	}
+}
+</script>
+
+<style lang='sass'>
+.settings
+	height: 95vh
+</style>
