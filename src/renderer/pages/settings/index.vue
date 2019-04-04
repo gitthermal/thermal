b6ef6869d9b212d93c9dfe16adc9ea21d6cd4471
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-04 14:38:03 +0530

    Add custom scroll for settings content

--- src/renderer/pages/settings/index.vue
+++ src/renderer/pages/settings/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="settings d-flex">
     <settingsSidebar/>
+		<div class="w-100">
+			<VueScrollbar class="settings__scrollbar">
+				<div class="container">
+					<div class="settings__content">
+						<router-view></router-view>
+					</div>
+				</div>
+			</VueScrollbar>
+		</div>
   </div>
 </template>
 
 <script>
 import settingsSidebar from "../../components/sidebar/settings"
+import VueScrollbar from "vue2-scrollbar"
 
 export default {
 	name: "settings",
 	components: {
 		settingsSidebar,
+		VueScrollbar
 	}
 }
 </script>
@@ -18,4 +29,11 @@ export default {
 <style lang='sass'>
 .settings
 	height: 95vh
+
+	&__scrollbar
+		max-height: 95vh
+
+	&__content
+		margin-top: 2rem
+		padding: 20px
 </style>
