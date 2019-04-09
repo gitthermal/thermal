a2aebd3f91d8d8cd4d5b601eb2ffee2ee2376248
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-09 16:38:47 +0530

    Return data instead of mouting

--- src/renderer/mixins/git/log.js
+++ src/renderer/mixins/git/log.js
@@ -1,19 +1,15 @@
 import git from "simple-git/promise";
 import * as Sentry from "@sentry/electron";
 
-const log = {
-	async mounted() {
-		const gitLog = await git(this.currentRepository.path).log();
-		try {
-			this.$store.dispatch("history/getRepositoryLogs", {
-				logs: gitLog.all
-			});
-		} catch (error) {
-			Sentry.captureException(error);
-			let errorMessage = "Unable to fetch logs.";
-			console.log(errorMessage);
-			Sentry.captureMessage(errorMessage, gitLog);
-		}
+const log = async repository => {
+	const data = await git(repository.path).log();
+	try {
+		return data.all;
+	} catch (error) {
+		Sentry.captureException(error);
+		let errorMessage = "Unable to fetch logs.";
+		console.log(errorMessage);
+		Sentry.captureMessage(errorMessage, data);
 	}
 };
 
