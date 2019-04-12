c0b6c1143efc17f2a97dfe1ea1ca232866601ace
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-12 22:55:25 +0530

    Turning off prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
 		__static: true
 	},
 	rules: {
+		"prettier/prettier": "off",
 		// allow debugger during development
 		"no-debugger": process.env.NODE_ENV === "testing" ? 2 : 0,
 		// enable semicolons instead of ASI
