ff0d5c0b281373008e37f00fd1a8a22990f83103
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-23 14:13:56 +0530

    Create fontProps.js

---
+++ src/renderer/mixins/fontProps.js
@@ -0,0 +1,34 @@
+export default {
+	props: {
+		// a prioritized list of one or more font family names 
+		fontFamily: {
+			type: String,
+			default: 'inherit'
+		},
+		// size of the font in rems
+		fontSize: {
+			type: Number,
+			default: 1
+		},
+		// specifies the weight (or boldness) of the font
+		fontWeight: {
+			type: Number,
+			default: 400
+		},
+		// amount of spacing between lines
+		lineHeight: {
+			type: Number,
+			default: 1
+		}
+	},
+	data() {
+		return {
+			borderProps: {
+				fontFamily: this.fontFamily,
+				fontSize: this.fontSize + "rem",
+				fontWeight: this.fontWeight,
+				lineHeight: this.lineHeight
+			}
+		};
+	}
+};
