b9d7440808ce55dfb619dde65089f953121bbe02
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-09 11:19:15 +0530

    Accept params for git logs

--- src/renderer/git/log.js
+++ src/renderer/git/log.js
@@ -1,8 +1,8 @@
 import git from "simple-git/promise";
 import * as Sentry from "@sentry/electron";
 
-const log = async repository => {
-	const data = await git(repository.path).log();
+const log = async (repository, params) => {
+	const data = await git(repository.path).log(params);
 	try {
 		return data.all;
 	} catch (error) {
