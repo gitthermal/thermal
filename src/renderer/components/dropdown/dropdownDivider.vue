90fd87427ddbaf614eebd1797d9a769969d1d770
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-17 20:42:34 +0530

    Dropdown components (#6)
    * Create dropdown divider component
    * Create dropdown item component
    * Create dropdown list component
    * Replace html with dropdown components
    * Click event in dropdown item components
    * Remove native click from logo
    * Auto close dropdown on mouse leave

---
+++ src/renderer/components/dropdown/dropdownDivider.vue
@@ -0,0 +1,11 @@
+<template>
+	<div
+		:style="{
+			marginTop: '6px',
+			marginBottom: '6px',
+			height: '1px',
+			width: '100%',
+			backgroundColor: 'rgba(55, 53, 47, 0.09)'
+		}"
+	/>
+</template>
