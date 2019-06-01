27ccf213bdfd87ceba3330461f6f1a18cbeb81ef
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-01 10:28:35 +0530

    Create share feedback modal

---
+++ src/renderer/components/model/feedback.vue
@@ -0,0 +1,36 @@
+<template>
+	<div v-show="this.$store.state.model.model.feedback" class="model--small">
+		<div class="model__section model__header">
+			<h6 class="model__header__title">
+				Share feedback
+			</h6>
+			<div class="model__header__close" @click="closeModel">
+				<close-icon />
+			</div>
+		</div>
+		<div class="model__section model__body">
+		</div>
+		<div class="model__section model__footer"></div>
+	</div>
+</template>
+
+<script>
+import closeIcon from "../icon/close";
+
+export default {
+	name: "FeedbackForm",
+	components: {
+		closeIcon,
+	},
+	methods: {
+		closeModel() {
+			this.$store.dispatch("model/showFeedback");
+		}
+	}
+};
+</script>
+
+<style lang="sass">
+
+</style>
+
