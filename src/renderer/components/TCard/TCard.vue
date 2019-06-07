6e7bf027b2cf50a128b836513658be7d9330311b
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-07 19:12:07 +0530

    Create card, header, body & footer component

---
+++ src/renderer/components/TCard/TCard.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="t-card" :style="{ maxWidth: cardSize }">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "TCard",
+	props: {
+		width: {
+			type: [String, Number],
+			default: "medium"
+		}
+	},
+	computed: {
+		cardSize() {
+			if (!isNaN(this.width)) {
+				return this.width + "px";
+			} else {
+				switch (this.width) {
+					case "small":
+						return "450px";
+					case "medium":
+					default:
+						return "700px";
+					case "large":
+						return "100%";
+				}
+			}
+		}
+	}
+};
+</script>
+
+<style lang="sass">
+.t-card
+	background-color: white
+	box-shadow: 0px 11px 15px -7px rgba(0,0,0,0.2), 0px 24px 38px 3px rgba(0,0,0,0.14), 0px 9px 46px 8px rgba(0,0,0,0.12)
+	border-radius: .35rem
+	margin-left: 20px
+	margin-right: 20px
+	width: 100%
+</style>
---
+++ src/renderer/components/TCard/TCardHeader.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="t-card__header">
+		<div class="t-card__header-heading">
+			{{ heading }}
+		</div>
+		<div class="t-card__header__close" @click="closeModel()">
+			<close-icon />
+		</div>
+	</div>
+</template>
+
+<script>
+import closeIcon from "../icon/close";
+
+export default {
+	name: "TCardHeader",
+	components: {
+		closeIcon
+	},
+	props: {
+		heading: {
+			type: String,
+			default: null
+		},
+		close: {
+			type: Boolean,
+			default: true
+		}
+	}
+};
+</script>
+
+<style lang="sass">
+.t-card__header
+	display: flex
+	border-bottom: 1px solid #eee
+	padding: 0.8rem 1rem
+
+	&__close
+		cursor: pointer
+		display: flex
+		margin-left: auto
+
+		svg
+			width: 20px
+			height: 20px
+			stroke: #7f8186
+</style>
