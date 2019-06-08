a255d4cbe9f4b8b51b726d5114038c73c188800e
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-08 10:49:07 +0530

    Split TCardHeader into TCardHeaderHeading & TCardHeaderClose

--- src/renderer/components/TCard/TCardHeader.vue
+++ src/renderer/components/TCard/TCardHeader.vue
@@ -1,32 +1,12 @@
 <template>
 	<div class="t-card__header">
-		<div class="t-card__header-heading">
-			{{ heading }}
-		</div>
-		<div class="t-card__header__close" @click="closeModel()">
-			<close-icon />
-		</div>
+		<slot />
 	</div>
 </template>
 
 <script>
-import closeIcon from "../icon/close";
-
 export default {
-	name: "TCardHeader",
-	components: {
-		closeIcon
-	},
-	props: {
-		heading: {
-			type: String,
-			default: null
-		},
-		close: {
-			type: Boolean,
-			default: true
-		}
-	}
+	name: "TCardHeader"
 };
 </script>
 
@@ -35,14 +15,4 @@ export default {
 	display: flex
 	border-bottom: 1px solid #eee
 	padding: 0.8rem 1rem
-
-	&__close
-		cursor: pointer
-		display: flex
-		margin-left: auto
-
-		svg
-			width: 20px
-			height: 20px
-			stroke: #7f8186
 </style>
---
+++ src/renderer/components/TCard/TCardHeaderClose.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="t-card__header-close" @click="closeModal(modalName)">
+		<close-icon />
+	</div>
+</template>
+
+<script>
+import CloseIcon from "../icon/close";
+import closeModalMixin from "../../mixins/closeModal";
+
+export default {
+	name: "TCardHeaderClose",
+	components: {
+		CloseIcon
+	},
+	mixins: [closeModalMixin],
+	props: {
+		modalName: {
+			type: String,
+			default: null
+		}
+	}
+};
+</script>
+
+<style lang="sass">
+.t-card__header
+	&-close
+		cursor: pointer
+		display: flex
+		margin-left: auto
+
+		svg
+			width: 20px
+			height: 20px
+			stroke: #7f8186
+</style>
