665544e4f5c32230abaf2140cb30263e9467a3c5
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-09 14:25:55 +0530

    Create scrollbar layout component

---
+++ src/renderer/components/TLayouts/TScrollbar.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<vue-scrollbar>
+			<slot />
+		</vue-scrollbar>
+	</div>
+</template>
+
+<script>
+import VueScrollbar from "vue2-scrollbar";
+
+export default {
+	name: "TScrollbarLayout",
+	components: {
+		VueScrollbar
+	}
+};
+</script>
