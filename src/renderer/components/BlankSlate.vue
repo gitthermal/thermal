95eca3b9dbb22c529ac1b7062f829e19e525c026
Yashu Mittal <mittalyashu77@gmail.com>
2019-06-16 18:24:31 +0530

    Create BlankSlate placeholder component

---
+++ src/renderer/components/BlankSlate.vue
@@ -0,0 +1,28 @@
+<template>
+	<t-flexbox
+		flex-direction="column"
+		justify-content="center"
+		align-items="center"
+		class="blankslate"
+	>
+		No content to show
+	</t-flexbox>
+</template>
+
+<script>
+import TFlexbox from "./TLayouts/TFlexbox";
+
+export default {
+	name: "BlankSlate",
+	components: {
+		TFlexbox
+	}
+};
+</script>
+
+<style lang="sass">
+.blankslate
+	width: 100%
+	font-size: 12px
+	color: #7A7D84
+</style>
