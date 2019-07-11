9b90c858324e4ccaea12e53c400f49cbefe4d466
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-11 18:03:58 +0530

    Use navigation guard beforeRouteEnter()

--- src/renderer/mixins/repositoryData.js
+++ src/renderer/mixins/repositoryData.js
@@ -4,6 +4,13 @@ export default {
 			repositoryData: {}
 		};
 	},
+	beforeRouteEnter(to, from, next) {
+		if (!isNaN(to.params.projectId)) {
+			next();
+		} else {
+			next(false);
+		}
+	},
 	mounted() {
 		this.repositoryData = this.$store.getters["repository/listAllRepository"][
 			this.$route.params.projectId
