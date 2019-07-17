7a4d5cac1c2e06dfcdd3c37ba5428a604ed73cb1
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-17 10:47:31 +0530

    Create git commands page

---
+++ src/renderer/pages/commands.vue
@@ -0,0 +1,16 @@
+<template>
+	<t-flexbox align-items="center">
+		<h1>Git Commands</h1>
+	</t-flexbox>
+</template>
+
+<script>
+import TFlexbox from "../components/TLayouts/TFlexbox";
+
+export default {
+	name: "GitCommands",
+	components: {
+		TFlexbox
+	}
+};
+</script>
