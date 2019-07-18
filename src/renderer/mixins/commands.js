c333d9343803062b38bda830cf1f7dfce21b3e77
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-18 18:04:20 +0530

    Mixin to commit the git command to vuex store

---
+++ src/renderer/mixins/commands.js
@@ -0,0 +1,8 @@
+import store from "../store";
+
+export default async (command, params) => {
+	store.commit("commands/addCommand", {
+		title: `git ${command} ${!!params === true ? params : ""}`,
+		path: store.getters["workspace/currentRepository"].path
+	});
+};
