ced980c5b79be6f58efdca6e8dbac4fc1aa237c3
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-18 18:04:34 +0530

    Import gitCommand component

--- src/renderer/pages/commands.vue
+++ src/renderer/pages/commands.vue
@@ -1,16 +1,40 @@
 <template>
-	<t-flexbox align-items="center">
-		<h1>Git Commands</h1>
+	<t-flexbox>
+		<t-container>
+			<h1>Git Commands</h1>
+			<git-command
+				v-for="(item, index) in fetchAllCommands"
+				:key="index"
+				:index="index"
+				:title="item.title"
+				:path="item.path"
+			/>
+		</t-container>
 	</t-flexbox>
 </template>
 
 <script>
 import TFlexbox from "../components/TLayouts/TFlexbox";
+import TContainer from "../components/TLayouts/TContainer";
+import GitCommand from "../components/gitCommand";
 
 export default {
 	name: "GitCommands",
 	components: {
-		TFlexbox
+		TFlexbox,
+		TContainer,
+		GitCommand
+	},
+	computed: {
+		fetchAllCommands() {
+			return this.$store.state.commands.commands;
+		}
 	}
 };
 </script>
+
+<style lang="sass" scoped>
+h1
+	margin-top: 3rem
+	margin-bottom: 1rem
+</style>
