91cc49c73e65d97d6903dd1f9937dc2bcc0f713f
Yashu Mittal <mittalyashu77@gmail.com>
2019-07-21 19:23:34 +0530

    Import gitCommand mixin to add command

--- src/renderer/git/diff.js
+++ src/renderer/git/diff.js
@@ -1,9 +1,11 @@
 import git from "simple-git/promise";
 import * as Sentry from "@sentry/electron";
+import gitCommand from "../mixins/commands";
 
 const diff = async (repository, params) => {
 	const data = await git(repository.path).diff(params);
 	try {
+		gitCommand("diff", params.join(" "));
 		return data;
 	} catch (error) {
 		Sentry.captureException(error);
