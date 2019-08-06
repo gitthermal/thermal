9850948748a67c627cbbaa4dc4c1b8a8bf0aa919
Yashu Mittal <mittalyashu77@gmail.com>
2019-08-06 16:29:44 +0530

    Export the open database connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,4 @@
-const sqlite3 = require("sqlite3").verbose()
+const sqlite3 = require("sqlite3").verbose();
+const database = new sqlite3.Database("thermal");
 
-const database = new sqlite3.Database("thermalapp")
-
-database.serialize(() => {
-	database.run("CREATE TABLE IF NOT EXISTS repositories(name VARCHAR(100), path VARCHAR(1000));")
-})
-
-database.serialize(() => {
-	database.run("INSERT into repositories(name, path) VALUES ('thermal', 'C:/Users/YASHU/Desktop/thermal')")
-})
-
-database.serialize(() => {
-	database.all("SELECT * from repositories", (err, rows) => {
-		if (err) {
-			console.log(err)
-		} else {
-			console.log(rows)
-		}
-	})
-})
+export default database;
