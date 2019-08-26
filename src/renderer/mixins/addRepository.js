038484649e1bd5c77a5946cdc56535eb25d45ea1
Yashu Mittal <mittalyashu77@gmail.com>
2019-08-26 11:49:18 +0530

    Valid git repository and getRemote URL

--- src/renderer/mixins/addRepository.js
+++ src/renderer/mixins/addRepository.js
@@ -12,18 +12,26 @@ export default {
 	},
 	methods: {
 		},
-		async isGitRepository(path) {
-			const validateGitRepository = git(path);
-			const isGit = await validateGitRepository.checkIsRepo();
-			try {
-				return isGit;
-			} catch (error) {
-				console.log(error);
-			}
 
 		// directory name
 		getRepositoryName(path) {
 			this.repository.name = path.split("/")[path.split("/").length - 1];
+		},
+
+		// get remote url
+		async getRemoteUrl(path) {
+			let listRemote;
+			try {
+				listRemote = await git(path).listRemote(["--get-url"]);
+				if (listRemote.slice(-4, -1) === "git") {
+					this.repository.isGitRepo = true;
+				}
+				this.repository.remote = listRemote;
+
+				console.log(listRemote);
+			} catch (error) {
+				console.log(error);
+			}
 		},
 				}
 			});
