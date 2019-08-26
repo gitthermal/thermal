02fc50d6657d1408c0077c014269023228437b55
Yashu Mittal <mittalyashu77@gmail.com>
2019-08-26 09:51:11 +0530

    Rename database file to thermal.sqlite

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
 const sqlite3 = require("sqlite3").verbose();
-const database = new sqlite3.Database("thermal");
+const database = new sqlite3.Database("thermal.sqlite");
 
 export default database;
