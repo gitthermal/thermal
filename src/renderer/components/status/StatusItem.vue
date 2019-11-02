5a2e09044c3057bf8bb432702c68304d253a3303
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-02 23:10:05 +0530

    Create status item component

---
+++ src/renderer/components/status/StatusItem.vue
@@ -0,0 +1,86 @@
+<template>
+	<t-flexbox align-items="center" class="status__item">
+		<input
+			class="status__item-checkbox"
+			type="checkbox"
+			:value="status.path"
+			@change="$emit('stagedFile', status.path)"
+		/>
+		<label :title="status.path" :for="status.path">
+			<t-flexbox>
+				<p class="status__item-pathname">
+					{{ filePath(status.path) }}
+				</p>
+				<p class="status__item-pathfile">
+					{{ fileName(status.path) }}
+				</p>
+			</t-flexbox>
+		</label>
+		<div
+			:style="`background-color: #${status.color}`"
+			class="status__item-type ml-auto"
+		>
+			{{ status.symbol }}
+		</div>
+	</t-flexbox>
+</template>
+
+<script>
+// components
+import TFlexbox from "../TLayouts/TFlexbox";
+
+export default {
+	name: "StatusItem",
+	components: {
+		TFlexbox
+	},
+	model: {
+		prop: "status.stage",
+		event: "stagedFile"
+	},
+	props: {
+		status: {
+			type: Object,
+			default: () => {
+				return {};
+			}
+		}
+	},
+	methods: {
+		filePath(path) {
+			if (path.lastIndexOf("/").toString() !== "-1") {
+				return path.slice(0, path.lastIndexOf("/"));
+			}
+		},
+		fileName(path) {
+			if (path.lastIndexOf("/").toString() !== "-1") {
+				return path.slice(path.lastIndexOf("/"), path.length);
+			}
+			return path;
+		}
+	}
+};
+</script>
+
+<style lang="sass">
+.status__item
+	border-bottom: 1px solid #DEE0E3
+	font-size: 12px
+	color: #2E3034
+	padding: 6px 10px
+	cursor: pointer
+
+	&:hover
+		background-color: rgba(#EFEFEF, 0.4)
+
+	&-checkbox
+		margin-right: 5px
+
+	&-pathname
+			color: #BEBEBE
+
+	&-type
+		padding: 3px 5px
+		color: white
+		border-radius: 3px
+</style>
