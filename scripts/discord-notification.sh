7d0f73a6c362a667710f2be5f215481d800f82a1
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-09 17:02:29 +0530

    Pass github context argument to discord-notification.sh
    $1: Webhook url
    $2: Workflow name
    $3: Organisation name/repository name
    $4: Ref (branch) name
    $5: Commit ID
    $6: Author name
    $7: Author image

--- scripts/discord-notification.sh
+++ scripts/discord-notification.sh
@@ -1,23 +1,46 @@
 #!/usr/bin/env bash
 
 # $1: Webhook url
+# $2: Workflow name
+# $3: Organisation name/repository name
+# $4: Ref (branch) name
+# $5: Commit ID
+# $6: Author name
+# $7: Author image
 
 DISCORD_WEBHOOK=$1
+WORKFLOW_NAME=$2
+REPOSITORY_ORG_NAME$3
+REF=$4
+COMMIT_ID=$5
+AUTHOR_NAME=$6
+AUTHOR_IMAGE=$7
 current_date=$(date --iso-8601=seconds)
+
+STATUS_COLOR=0x2664261
 
 curl \
 	--header "content-type: application/json" \
 	--request POST \
 	--data '{
-		"content": "**Build status**",
+		"username": "GitHub"
+		"avatar_url": "https://i.imgur.com/u6mj8bs.png",
+		"content": "**'"$WORKFLOW_NAME"'" build status**",
 		"embeds": [
 			{
 				"description": "Build [4534633]() is successful `origin/master`",
-				"color": 7816110,
+				"color": "'"$STATUS_COLOR"'",
 				"timestamp": "'"$current_date"'",
-				"footer": {
-					"text": "GitHub Action - CI Workflow"
-				}
+				"author": {
+					"name": "'"$AUTHOR_NAME"'",
+					"image": "'"$AUTHOR_IMAGE"'"
+				},
+				"fields": [
+					{
+						"name": "Commit ID",
+						"value": "["'"$COMMIT_ID"'"](https://www.github.com/"'"$REPOSITORY_ORG_NAME"'"/commit/"'"$COMMIT_ID"'")"
+					}
+				]
 			}
 		]
 	}' $DISCORD_WEBHOOK
