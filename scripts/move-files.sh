d912bbe015252e679ff4a6c8ac6b83d84f174ff5
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-09 09:58:27 +0530

    Update relative paths

--- scripts/move-files.sh
+++ scripts/move-files.sh
@@ -9,13 +9,13 @@ bash ./scripts/print-directory-content.sh dist ./dist
 # artifacts directory name
 directoryName=artifacts
 echo Create artifacts directory name: $directoryName
-mkdir $directoryName
-ls
+mkdir ./dist/$directoryName
+ls ./dist/
 printf "\n"
 
 # moving directory content
 echo Moving content to artifacts directory
-mv --verbose thermal-$1* --target-directory $directoryName
+mv --verbose ./dist/thermal-$1* --target-directory ./dist/$directoryName
 
 # list artifacts directory content
 bash ./scripts/print-directory-content.sh artifacts ./dist/artifacts
