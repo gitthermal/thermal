a76aad0683b017e20c41c693d399ef982cd30589
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-09 08:57:32 +0530

    move linux build app to artifacts directory

--- scripts/moveFiles.sh
+++ scripts/moveFiles.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-cd dist
-ls
+echo "Executing ${0}"
+printf "\n"
+
+# list directory content
+cd ./dist
+echo List directory content
+
+for entry in "$search_dir"./*
+do
+  echo $entry
+done
+printf "\n"
+
+# artifacts directory name
+directoryName=artifacts
+echo Crate artifacts directory name: $directoryName
+mkdir $directoryName
+printf "\n"
+
+# moving directory content
+echo Moving content to artifacts directory
+mv --verbose thermal-$1* --target-directory $directoryName
