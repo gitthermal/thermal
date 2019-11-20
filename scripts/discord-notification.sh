cdfaf2fe42118208a3ab378a8d22e17ee3cb501d
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-20 19:41:06 +0530

    Use getopts for passing flags with value

--- scripts/discord-notification.sh
+++ scripts/discord-notification.sh
@@ -1,23 +1,53 @@
 #!/usr/bin/env bash
 
-# $1: Webhook url
-# $2: Commit message
-# $3: Organisation name/repository name
-# $4: Ref (branch) name
-# $5: Commit ID
-# $6: Author name
-# $7: Author image
+# -u: Webhook url
+DISCORD_WEBHOOK=""
 
-DISCORD_WEBHOOK=$1
-COMMIT_MESSAGE=$2
-REPOSITORY_ORG_NAME=$3
-REF=$4
-COMMIT_ID=$5
-AUTHOR_NAME=$6
-AUTHOR_IMAGE=$7
+# -m: Commit message
+COMMIT_MESSAGE=""
+
+# -r: Organisation name/repository name
+REPOSITORY_ORG_NAME=""
+
+# -b: Ref (branch) name
+REF=""
+
+# -c: Commit ID
+COMMIT_ID=""
+
+# -n: Author name
+AUTHOR_NAME=""
+
+# -i: Author image
+AUTHOR_IMAGE=""
+
 current_date=$(date --iso-8601=seconds)
 
 STATUS_COLOR=0x2664261
+
+while getopts "u:m:r:b:c:n:i" opt; do
+	case $opt in
+		u)
+			DISCORD_WEBHOOK=$OPTARG
+			;;
+		m)
+			COMMIT_MESSAGE=$OPTARG
+		r)
+			REPOSITORY_ORG_NAME=$OPTARG
+		b)
+			REF=$OPTARG
+		c)
+			COMMIT_ID=$OPTARG
+		n)
+			AUTHOR_NAME=$OPTARG
+		i)
+			AUTHOR_IMAGE=$OPTARG
+		\?)
+			help_func
+			exit 1
+			;;
+	esac
+done
 
 # echo all variables
 echo $DISCORD_WEBHOOK
@@ -58,3 +88,23 @@ curl \
 			}
 		]
 	}' $DISCORD_WEBHOOK
+
+function help_func() {
+	echo "Discord notification - Send notifications to Discord"
+	printf "\n"
+
+	echo "Usage: ./scripts/discord-notification.sh <command>"
+	printf "\n"
+
+	echo "where <command> is one of:"
+	echo "u, m, r, b, c, n, i"
+	printf "\n"
+
+	echo "-u		Discord webhook url"
+	echo "-m		Commit message"
+	echo "-r		Organization and repository name"
+	echo "-b		Reference branch nane"
+	echo "-c		Long hash (commit ID)"
+	echo "-n		Sender or author name"
+	echo "-i		Sender or author image"
+}
